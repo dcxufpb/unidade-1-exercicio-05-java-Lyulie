8e7a6755376a90a6e432149109abd905d9312ffa
Rodolpho Alves <rrpa15@live.com>
2020-09-21 15:17:24 -0300

    vs

--- src/test/java/com/example/project/TestCupomFiscal.java
+++ src/test/java/com/example/project/TestCupomFiscal.java
@@ -2,6 +2,7 @@ package com.example.project;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+// import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class TestCupomFiscal {
